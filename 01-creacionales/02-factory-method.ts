928a428c4e2eb14fc99101407b48fc3a4bc2e6d5
Crhistian Alexander Garcia <c.garcia@izertis.com>
2026-06-17 16:49:00 +0200

    factory

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,83 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+import { sleep } from "../helpers/sleep.ts";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class CheeseBurger implements Hamburger {
+  prepare(): void {
+    console.log("...Preparing a %cCheese Burger", COLORS.yellow);
+  }
+}
+
+class VeggieBurger implements Hamburger {
+  prepare(): void {
+    console.log("...Preparing a %cVeggie Burger", COLORS.green);
+  }
+}
+
+abstract class BurgerFactory {
+  protected abstract createBurger(): Hamburger;
+
+  async orderBurger(): Promise<Hamburger> {
+    console.log("Ordering a burger...");
+    await sleep(1000);
+    const burger = this.createBurger();
+    burger.prepare();
+    return burger;
+  }
+}
+
+class CheeseBurgerFactory extends BurgerFactory {
+  override createBurger(): Hamburger {
+    return new CheeseBurger();
+  }
+}
+
+class VeggieBurgerFactory extends BurgerFactory {
+  override createBurger(): Hamburger {
+    return new VeggieBurger();
+  }
+}
+
+async function main() {
+  let factory: BurgerFactory | undefined = undefined;
+
+  const burguerType = prompt(
+    "What type of burger would you like? (cheese/veggie)",
+  )
+    ?.trim()
+    .toLowerCase();
+  console.log("Selected burger: ", burguerType);
+
+  if (burguerType !== "cheese" && burguerType !== "veggie") {
+    throw new Error("Invalid burger type. Must be 'cheese' or 'veggie'.");
+  }
+
+  if (burguerType === "veggie") {
+    factory = new VeggieBurgerFactory();
+  }
+  if (burguerType === "cheese") {
+    factory = new CheeseBurgerFactory();
+  }
+
+  if (!factory) {
+    throw new Error("Factory is undefined. Cannot create burger.");
+  }
+
+  const burger = await factory.orderBurger();
+
+  if (burger) {
+    console.log(
+      `%c${burguerType} 🍔 ready to go! Enjoy your meal!`,
+      COLORS.purple,
+    );
+  }
+}
+
+main();
--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -12,19 +12,19 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * 	!Descripción:
   1.	Completen las clases SalesReport e InventoryReport para implementar 
       la interfaz Report, generando el contenido de cada reporte en el método generate.
 	  
-  2.	Implementen las clases SalesReportFactory e InventoryReportFactory 
-      para crear instancias de SalesReport y InventoryReport, respectivamente.
+    2.	Implementen las clases SalesReportFactory e InventoryReportFactory 
+        para crear instancias de SalesReport y InventoryReport, respectivamente.
 
 	3.	Prueben el programa generando diferentes tipos de reportes usando
       el prompt para seleccionar el tipo de reporte.
  */
-
-import { COLORS } from '../helpers/colors.ts';
 
 // 1. Definir la interfaz Report
 interface Report {
@@ -35,19 +35,21 @@ interface Report {
 // Implementar SalesReport e InventoryReport
 
 class SalesReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de ventas...'
+  generate(): void {
+    console.log("%cGenerando reporte de ventas...", COLORS.gray);
+  }
 }
 
 class InventoryReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log("%cGenerando reporte de inventario...", COLORS.brown);
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
@@ -58,14 +60,14 @@ abstract class ReportFactory {
 // 4. Clases Concretas de Fábricas de Reportes
 
 class SalesReportFactory extends ReportFactory {
-  createReport(): Report {
-    throw new Error('Method not implemented.');
+  override createReport(): Report {
+    return new SalesReport();
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
-  createReport(): Report {
-    throw new Error('Method not implemented.');
+  override createReport(): Report {
+    return new InventoryReport();
   }
 }
 
@@ -74,13 +76,12 @@ class InventoryReportFactory extends ReportFactory {
 function main() {
   let reportFactory: ReportFactory;
 
-  const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
-    COLORS.red
-  );
+  const reportType = prompt("¿Qué tipo de reporte deseas? (sales/inventory)");
 
-  if (reportType === 'sales') {
+  if (reportType === "sales") {
     reportFactory = new SalesReportFactory();
+  } else if (reportType === "inventory") {
+    reportFactory = new InventoryReportFactory();
   } else {
     reportFactory = new InventoryReportFactory();
   }
