5af805984585922148910abf355767c39830ca13
Crhistian Alexander Garcia <c.garcia@izertis.com>
2026-06-17 13:54:56 +0200

    builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,62 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+  protected cpu = " cpu - not defined";
+  protected ram = " ram - not defined";
+  protected storage = " storage - not defined";
+  protected gpu? = " gpu - not defined";
+
+  displayConfig(color: string = "color: white"): void {
+    console.log(`%cComputer configuration:`, `${color}; font-weight: bold;`);
+
+    console.log(`    CPU: ${this.cpu}
+    RAM: ${this.ram}
+    Storage: ${this.storage}
+    GPU: ${this.gpu}`);
+  }
+}
+
+class ComputerBuilder extends Computer {
+  setCPU(cpu: string): ComputerBuilder {
+    this.cpu = cpu;
+    return this;
+  }
+
+  setRAM(ram: string): ComputerBuilder {
+    this.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.storage = storage;
+    return this;
+  }
+
+  setGPU(gpu: string): ComputerBuilder {
+    this.gpu = gpu;
+    return this;
+  }
+
+  build(): Computer {
+    return this;
+  }
+}
+
+// Uso del patrón Builder
+
+function main() {
+  const computer = new ComputerBuilder()
+    .setCPU("Intel i9")
+    .setRAM("32GB")
+    .setStorage("1TB SSD")
+    .setGPU("NVIDIA RTX 3090")
+    .build();
+
+  computer.displayConfig(COLORS.green);
+}
+
+main();
--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -11,7 +11,7 @@
  * * que lo componen.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 //! Tarea: crear un QueryBuilder para construir consultas SQL
 /**
@@ -50,37 +50,73 @@ class QueryBuilder {
   }
 
   select(...fields: string[]): QueryBuilder {
-    throw new Error('Method not implemented.');
+    if (fields.length === 0) {
+      this.fields = ["*"];
+    } else {
+      this.fields = fields;
+    }
+    return this;
+    // throw new Error('Method not implemented.');
   }
 
   where(condition: string): QueryBuilder {
-    throw new Error('Method not implemented.');
+    if (!condition) {
+      throw new Error("Condition cannot be empty.");
+    }
+    this.conditions.push(condition);
+    return this;
   }
 
-  orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
-    throw new Error('Method not implemented.');
+  orderBy(field: string, direction: "ASC" | "DESC" = "ASC"): QueryBuilder {
+    if (!field || (direction !== "ASC" && direction !== "DESC")) {
+      throw new Error(
+        "Field cannot be empty and direction must be 'ASC' or 'DESC'.",
+      );
+    }
+
+    this.orderFields.push(`${field} ${direction}`);
+    return this;
   }
 
   limit(count: number): QueryBuilder {
-    throw new Error('Method not implemented.');
+    if (count <= 0) {
+      throw new Error("Limit must be a positive number.");
+    }
+    this.limitCount = count;
+    return this;
   }
 
   execute(): string {
     // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-    throw new Error('Method not implemented.');
+    const selectClause = `SELECT ${this.fields.join(", ")} FROM ${this.table}`;
+
+    const whereClause =
+      this.conditions.length > 0
+        ? `WHERE ${this.conditions.join(" AND ")}`
+        : "";
+    const orderByClause =
+      this.orderFields.length > 0
+        ? `ORDER BY ${this.orderFields.join(", ")}`
+        : "";
+    const limitClause = this.limitCount ? `LIMIT ${this.limitCount}` : "";
+
+    return `${selectClause} ${whereClause} ${orderByClause} ${limitClause}`
+      .replace(/\s+/g, " ")
+      .trim()
+      .concat(";");
   }
 }
 
 function main() {
-  const usersQuery = new QueryBuilder('users')
-    .select('id', 'name', 'email')
-    .where('age > 18')
+  const usersQuery = new QueryBuilder("users")
+    .select("id", "name", "email")
+    .where("age > 18")
     .where("country = 'Cri'") // Esto debe de hacer una condición AND
-    .orderBy('name', 'ASC')
+    .orderBy("name", "DESC")
     .limit(10)
     .execute();
 
-  console.log('%cConsulta:\n', COLORS.red);
+  console.log("%cConsulta:\n", COLORS.blue);
   console.log(usersQuery);
 }
 
