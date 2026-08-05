874d7c672e1871391d877d7d1dfa0c6327169871
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2026-08-06 00:24:17 +0200

    feature: factory-function

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -1,3 +1,4 @@
+import { COLORS } from "../helpers/colors.ts";
 /**
  * ! Factory Function
  * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
@@ -7,3 +8,29 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+type Language = "es" | "en" | "fr";
+
+function createGreeter(lang: Language) {
+  return function (name: String) {
+    const message = {
+      es: `Hola %c${name}`,
+      en: `Hi %c${name}`,
+      fr: `Bonjour %c${name}`,
+    };
+
+    return console.log(message[lang], COLORS.cyan);
+  };
+}
+
+function main() {
+  const names = ["Marco", "Laura"];
+  const langs: Language[] = ["es", "en", "fr"];
+  for (const name of names) {
+    for (const lang of langs) {
+      createGreeter(lang)(name); //Currying
+    }
+  }
+}
+
+main();
